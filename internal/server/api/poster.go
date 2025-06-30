e2fcc7d56063d80df0ce438a5081bb506dda5fa7
Topvennie <vincent@vallaeys.com>
2025-07-01 00:47:13 +0200

    feat(backend): posters

---
+++ internal/server/api/poster.go
@@ -0,0 +1,121 @@
+package api
+
+import (
+	"github.com/ZeusWPI/events/internal/server/dto"
+	"github.com/ZeusWPI/events/internal/server/service"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Poster struct {
+	router fiber.Router
+
+	poster service.Poster
+}
+
+func NewPoster(router fiber.Router, service service.Service) *Poster {
+	api := &Poster{
+		router: router.Group("/poster"),
+		poster: *service.NewPoster(),
+	}
+
+	api.createRoutes()
+
+	return api
+}
+
+func (r *Poster) createRoutes() {
+	r.router.Get("/:id/file", r.getFile)
+	r.router.Delete("/:id", r.delete)
+	r.router.Post("/:id", r.update)
+	r.router.Put("/", r.create)
+}
+
+func (r *Poster) getFile(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	file, err := r.poster.GetFile(c.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	c.Set("Content-Type", mimePNG)
+	return c.Send(file)
+}
+
+func (r *Poster) create(c *fiber.Ctx) error {
+	var poster dto.PosterSave
+	if err := c.BodyParser(&poster); err != nil {
+		return fiber.ErrBadRequest
+	}
+	if poster.ID != 0 {
+		return fiber.ErrBadRequest
+	}
+
+	form, err := c.MultipartForm()
+	if err != nil {
+		return fiber.ErrBadRequest
+	}
+	file, err := getFormFile(form, "file")
+	if err != nil {
+		return err
+	}
+
+	poster.File = file
+
+	if err := dto.Validate.Struct(poster); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	if _, err := r.poster.Save(c.Context(), poster); err != nil {
+		return err
+	}
+
+	return c.SendStatus(fiber.StatusCreated)
+}
+
+func (r *Poster) update(c *fiber.Ctx) error {
+	var poster dto.PosterSave
+	if err := c.BodyParser(&poster); err != nil {
+		return fiber.ErrBadRequest
+	}
+	if poster.ID == 0 {
+		return fiber.ErrBadRequest
+	}
+
+	form, err := c.MultipartForm()
+	if err != nil {
+		return fiber.ErrBadRequest
+	}
+	file, err := getFormFile(form, "file")
+	if err != nil {
+		return err
+	}
+
+	poster.File = file
+
+	if err := dto.Validate.Struct(poster); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	if _, err := r.poster.Save(c.Context(), poster); err != nil {
+		return err
+	}
+
+	return c.SendStatus(fiber.StatusCreated)
+}
+
+func (r *Poster) delete(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	if err := r.poster.Delete(c.Context(), id); err != nil {
+		return err
+	}
+
+	return c.SendStatus(fiber.StatusNoContent)
+}
---
+++ internal/server/service/poster.go
@@ -0,0 +1,98 @@
+package service
+
+import (
+	"context"
+
+	"github.com/ZeusWPI/events/internal/db/model"
+	"github.com/ZeusWPI/events/internal/db/repository"
+	"github.com/ZeusWPI/events/internal/server/dto"
+	"github.com/ZeusWPI/events/pkg/storage"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+type Poster struct {
+	service Service
+
+	poster repository.Poster
+}
+
+func (s *Service) NewPoster() *Poster {
+	return &Poster{
+		service: *s,
+		poster:  *s.repo.NewPoster(),
+	}
+}
+
+func (p *Poster) GetFile(ctx context.Context, posterID int) ([]byte, error) {
+	poster, err := p.poster.Get(ctx, posterID)
+	if err != nil {
+		zap.S().Error(err)
+		return nil, fiber.ErrInternalServerError
+	}
+	if poster == nil {
+		return nil, fiber.ErrBadRequest
+	}
+
+	file, err := storage.S.Get(poster.FileID)
+	if err != nil {
+		zap.S().Error(err)
+		return nil, fiber.ErrInternalServerError
+	}
+
+	return file, nil
+}
+
+func (p *Poster) Save(ctx context.Context, posterSave dto.PosterSave) (dto.Poster, error) {
+	poster := model.Poster{
+		ID:      posterSave.ID,
+		EventID: posterSave.EventID,
+		SCC:     posterSave.SCC,
+	}
+
+	if poster.ID != 0 {
+		// Update, delete old poster
+		oldPoster, err := p.poster.Get(ctx, poster.ID)
+		if err != nil {
+			zap.S().Error(err)
+			return dto.Poster{}, fiber.ErrInternalServerError
+		}
+		if oldPoster == nil {
+			return dto.Poster{}, fiber.ErrBadRequest
+		}
+
+		if err = storage.S.Delete(oldPoster.FileID); err != nil {
+			zap.S().Error(err)
+			return dto.Poster{}, fiber.ErrInternalServerError
+		}
+	}
+
+	poster.FileID = uuid.NewString()
+	if err := storage.S.Set(poster.FileID, posterSave.File, 0); err != nil {
+		zap.S().Error(err)
+		return dto.Poster{}, fiber.ErrInternalServerError
+	}
+
+	var err error
+	if poster.ID == 0 {
+		err = p.poster.Create(ctx, &poster)
+	} else {
+		err = p.poster.Update(ctx, poster)
+	}
+	if err != nil {
+		zap.S().Error(err)
+		return dto.Poster{}, fiber.ErrInternalServerError
+	}
+
+	return dto.PosterDTO(&poster), nil
+}
+
+func (p *Poster) Delete(ctx context.Context, posterID int) error {
+	if err := p.poster.Delete(ctx, posterID); err != nil {
+		zap.S().Error(err)
+		return fiber.ErrInternalServerError
+	}
+
+	return nil
+}
