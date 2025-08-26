124b26ae557501dc38e284d129cc29d8188cf023
Topvennie <vincent@vallaeys.com>
2025-08-26 17:26:46 +0200

    fix(avatar): add missing avatar

--- ui/src/components/organisms/navigation/NavUser.tsx
+++ ui/src/components/organisms/navigation/NavUser.tsx
@@ -1,16 +1,14 @@
-import { ChevronsUpDown, LogOut, Moon, Sun } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { OrganizerIcon } from "@/components/atoms/OrganizerIcon";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
 import { useAuth } from "@/lib/hooks/useAuth";
 import { useTheme } from "@/lib/hooks/useTheme";
+import { ChevronsUpDown, LogOut, Moon, Sun } from "lucide-react";
 
 export function NavUser() {
   const { user, logout } = useAuth();
   const { theme, setTheme } = useTheme();
   const { isMobile } = useSidebar();
-
-  const initials = user?.name.split(" ").map(n => n[0]).join("");
 
   const handleThemeSwitch = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -22,10 +20,7 @@ export function NavUser() {
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton size="lg" className="hover:cursor-pointer">
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={`https://zpi.zeus.gent/image/${user?.zauthId}`} alt={initials} />
-                <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
-              </Avatar>
+              <OrganizerIcon user={user!} />
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user?.name}</span>
                 <span className="truncate text-xs">{user?.role}</span>
@@ -41,10 +36,7 @@ export function NavUser() {
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage alt={initials} />
-                  <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
-                </Avatar>
+                <OrganizerIcon user={user!} />
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{user?.name}</span>
                   <span className="truncate text-xs">{user?.role}</span>
