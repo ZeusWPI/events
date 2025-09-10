f2562b1449260005df99385a62ca585c3bb72936
Topvennie <vincent@vallaeys.com>
2025-09-10 21:39:11 +0200

    feat(event): add detail api endpoint

--- ui/src/lib/api/event.ts
+++ ui/src/lib/api/event.ts
@@ -1,15 +1,25 @@
 import type { Event } from "../types/event";
 import type { Year } from "../types/year";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { convertEventsToModel } from "../types/event";
+import { convertEventsToModel, convertEventToModel } from "../types/event";
 import { apiGet, apiPost } from "./query";
 
 const ENDPOINT = "event";
 const STALE_5_MIN = 5 * 60 * 1000;
 
-export function useEventByYear({ id }: Pick<Year, "id">) {
+export function useEvent({ id }: Pick<Event, "id">) {
   return useQuery({
     queryKey: ["event", id],
+    queryFn: async () => (await apiGet(`${ENDPOINT}/${id}`, convertEventToModel)).data,
+    staleTime: STALE_5_MIN,
+    throwOnError: true,
+    enabled: id > 0,
+  })
+}
+
+export function useEventByYear({ id }: Pick<Year, "id">) {
+  return useQuery({
+    queryKey: ["event", "year", id],
     queryFn: async () => (await apiGet(`${ENDPOINT}/year/${id}`, convertEventsToModel)).data,
     staleTime: STALE_5_MIN,
     throwOnError: true,
@@ -22,14 +32,6 @@ export function useEventSaveOrganizers() {
 
   return useMutation({
     mutationFn: async (eventOrganizers: Pick<Event, 'id' | 'organizers' | 'year'>[]) => apiPost(`${ENDPOINT}/organizers`, eventOrganizers.map(e => ({ eventId: e.id, organizers: e.organizers.map(o => o.id) }))),
-    onSuccess: (_, events) => {
-      const uniqueYears = Array.from(
-        new Set(events.map(event => event.year.id)),
-      );
-
-      uniqueYears.forEach((yearId) => {
-        void queryClient.invalidateQueries({ queryKey: ["event", yearId] });
-      });
-    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["event"] })
   });
 }
